ef28a03b324d02a891c8b448c6d457c9bf02f5da
Charan J Singh <charanjsingh29@gmail.com>
2025-05-13 10:26:09 +0530

    Extra files removed

--- shared/publisher/events/index.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('events');
-const eventBus = new EventEmitter();
-
-const send = (event, payload) => eventBus.emit(event, payload);
-
-const listen = (event, callback) => eventBus.on(event, callback);
-
-module.exports = { send, listen };
